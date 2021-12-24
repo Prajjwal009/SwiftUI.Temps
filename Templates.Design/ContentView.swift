4c20d75958ba86b7dde8d43e6cb6f0358ad3056f
Prajjwal009 <sinhaprajjwal009@gmail.com>
2021-12-24 17:37:49 +0530

    Initial Commit

---
+++ Templates.Design/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Templates.Design
+//
+//  Created by Prajjwal on 24/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Templates.Design/Templates_DesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  Templates_DesignApp.swift
+//  Templates.Design
+//
+//  Created by Prajjwal on 24/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Templates_DesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
